2ade85cb3232ec4e0eb023febf8b539391beaf9a
aersosi <aersosi@avodaq.com>
2024-10-20 17:46:05 +0200

    [feature] blog page & mockup posts

---
+++ src/content/config.ts
@@ -0,0 +1,29 @@
+import { defineCollection, z } from 'astro:content';
+
+// Transform German date string (DD.MM.YYYY) to Date object
+const germanDateToDate = (dateString: string) => {
+  const [day, month, year] = dateString.split('.');
+  return new Date(`${year}-${month}-${day}`);
+};
+
+const blog = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.preprocess(
+			(val) => germanDateToDate(val as string),
+			z.date()
+		),
+		updateDate: z.preprocess(
+			(val) => germanDateToDate(val as string),
+			z.date()
+		).optional(),
+		heroImage: z.string().optional(),
+		heroAltText: z.string().optional(),
+	}),
+});
+
+export const collections = { blog };
