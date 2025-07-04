34cc966914cfc983f512e02754fcc33bf09e0329
aersosi <acerelroy@gmail.com>
2025-07-04 17:38:52 +0200

    rebuid: rebuild with blog

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from "astro:content";
+const postsCollection = defineCollection({
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      image: z.object({
+        url: image(),
+        alt: z.string(),
+      }),
+      tags: z.array(z.string()),
+    }),
+});
+export const collections = {
+  posts: postsCollection,
+};
