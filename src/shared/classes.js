191347a0449199069fca5e5c629c1a22f96bd981
aersosi <acerelroy@gmail.com>
2025-07-17 17:11:07 +0200

    refactor: remove CV.astro and standardize typography variants
    - Deleted <CV.astro> and associated dependencies from landing components.
    - Updated text variants (textXL to text2XL, textSM to textBase) across multiple components for consistency.
    - Refactored <Wrapper /> to use enhanced class handling with detailed prose scaling.
    - Abstracted grid and padding classes into shared/classes.js for reusability.
    - Removed redundant script and unused styles from global elements.

---
+++ src/shared/classes.js
@@ -0,0 +1,21 @@
+export const stackGrid = [
+    "grid",
+    "grid-cols-1",
+    "lg:grid-cols-3",
+
+    "gap-x-12",
+    "gap-y-8",
+
+    "border-base-400",
+    "dark:border-base-600",
+].join(" ");
+
+
+export const stackGridPadding = [
+    "py-20",
+    "lg:py-22",
+].join(" ");
+
+
+
+
