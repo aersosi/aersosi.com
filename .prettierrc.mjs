9daa2cba58888f8433cdaecbe52a0750899fe3c0
aersosi <acerelroy@gmail.com>
2025-07-18 23:30:57 +0200

    chore: add linting and formatting scripts and dependencies
    - Added "lint", "lint:fix", "format", and "format:check" scripts to package.json
    - Added eslint and prettier dependencies in devDependencies
    - Updated package-lock.json with new dependencies including eslint plugins and prettier plugins

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+export default {
+    printWidth: 100,
+    useTabs: true,
+    plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+    overrides: [
+        {
+            files: ["**/*.astro"],
+            options: {
+                parser: "astro",
+            },
+        },
+    ],
+};
