9c02d8763c9516ceda949263964a58b17baff4f6
aersosi <acerelroy@gmail.com>
2025-08-01 09:42:04 +0200

    feat(seo, metadata): remove Seo_new component and update meta details
    - Delete `Seo_new.astro` and its related code for cleanup.
    - Update `pageDescription` across multiple components and layouts.
    - Modify `robots.txt` to include `Sitemap` URL and `Allow` directive.
    - Update `site.webmanifest` with revised metadata and new theme color.
    - Adjust `<meta>` theme color in `Meta.astro` for consistency.

--- src/content/pages/about.ts
+++ src/content/pages/about.ts
@@ -3,7 +3,7 @@ export const aboutPageData = {
 	viewerDescription: "Lorem Ipsum dolor sit amet",
 	pageTitle: "Über mich | Arthur Ersosi",
 	pageDescription:
-		"Frontend-Developer und UI/UX-Designer mit 10+ Jahren Design Expertise und 8+ Jahren Web-Development Erfahrung aus Hamburg.",
+		"Frontend-Developer und UI/UX-Designer mit 9+ Jahren Design Expertise und 7+ Jahren Web-Development Erfahrung aus Hamburg.",
 	headerImgAlt: "Über mich Image Beschreibung",
 };
 
